09d480c55d44fa50589716502634f20b2dc10be7
DH-NB-666 <zb88666@qq.com>
2025-06-23 04:09:40 +0800

    Create _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-dan-huang_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-.lua

---
+++ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-dan-huang_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-.lua
@@ -0,0 +1,5 @@
+--此脚本公开源码
+--随意使用
+--注意查看
+--蛋皇现实人不行
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DH-NB-666/Dan_Huang_-_-_-_-_-_-_-_-_-_-_-_-_-_NB666/main/-_-_-_-_-_-_-_-_-_-_-_dan_huang_-_-_-_-_-_-_-_-_-_-_-_-.lua", true))()
